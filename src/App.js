aa694ed075c8d4062a776480dbe47ec294f106d4
sanju50729 <sanju99658@gmail.com>
2024-11-18 11:01:32 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Maincontent from './components/Maincontent';
-import './Style/header.css';  // Include global styles here
-import './Style/footer.css';  // Include footer styles here
+import './Style/header.css';  
+import './Style/footer.css';  
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
-import Footer from './components/Footer';
+
 
 
 
